0552f401dbbd4e10c61e9e1d3b0b7926fdde94ec
lanuioe <lanuioe@gmail.com>
2024-01-21 23:29:42 +0900

    [Refactor] 이미지 데이터바인딩

--- mission01/src/main.jsx
+++ mission01/src/main.jsx
@@ -1,34 +1,39 @@
 import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 import "/src/styles/tailwind.css";
+
 const root = document.getElementById("root");
 const reactDomRoot = ReactDOM.createRoot(root);
 
-const createPopup = (
-  <dialog className="popup overflow-hidden rounded-2xl p-0" open>
-    <a href="#">
-      <img
-        src="/images/popup.jpg"
-        alt="처음 만나는 뷰티 플랫폼 BEAUTY Karly 지금 바로 만나보세요!"
-      />
-    </a>
-    <div className="grid grid-flow-col grid-cols-2">
-      <button
-        type="button"
-        value="cancel"
-        className="button__today--closed border-r border-gray-100 py-[30px] text-p-base text-content"
-      >
-        오늘 하루 안 보기
-      </button>
-      <button
-        type="button"
-        value="cancel"
-        className="button--closed py-[30px] text-p-base text-content"
-      >
-        닫기
-      </button>
-    </div>
-  </dialog>
-);
+const popupImage = {
+  src: "/images/popup.jpg",
+  alt: "처음 만나는 뷰티 플랫폼 BEAUTY Karly 지금 바로 만나보세요!",
+};
 
-reactDomRoot.render(createPopup);
+const createPopup = () => {
+  return (
+    <dialog id="popup" className="popup overflow-hidden rounded-2xl p-0" open>
+      <a href="#">
+        <img src={popupImage.src} alt={popupImage.alt} />
+      </a>
+      <div className="grid grid-flow-col grid-cols-2">
+        <button
+          type="button"
+          value="cancel"
+          className="button__today--closed border-r border-gray-100 py-[30px] text-p-base text-content"
+        >
+          오늘 하루 안 보기
+        </button>
+        <button
+          type="button"
+          value="cancel"
+          className="button--closed py-[30px] text-p-base text-content"
+        >
+          닫기
+        </button>
+      </div>
+    </dialog>
+  );
+};
+
+reactDomRoot.render(createPopup());
