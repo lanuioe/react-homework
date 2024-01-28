66ce225d3cd0bf24d8f4cec78b5637e0bb419e7e
lanuioe <lanuioe@gmail.com>
2024-01-28 22:47:19 +0900

    [Refactor] 데이터 렌더링 시 구조분해할당 사용

--- mission02/src/components/product.jsx
+++ mission02/src/components/product.jsx
@@ -17,57 +17,53 @@ export default function RollingBanner() {
         <p className="mb-10 text-h-xl text-center">이 상품 어때요?</p>
         <Swiper
           slidesPerView={4}
-          spaceBetween={18}
           slidesPerGroup={4}
+          spaceBetween={18}
           navigation={true}
           modules={[Navigation]}
           className="recommendProduct justify-between"
         >
           {productData.items?.map((slideData) => {
+            const { id, src, alt, name, detail, discount, price } = slideData;
             return (
-              <SwiperSlide key={slideData.id} className="w-[249px]">
-                <div className="h-[320px] overflow-hidden">
-                  <img
-                    className="w-full"
-                    src={`../src/assets/product/product_thumb${slideData.src}.webp`}
-                    alt={slideData.alt}
-                  />
-                </div>
-                <span className="inline-block mt-4 text-l-sm text-gray-400">
-                  샛별배송
-                </span>
-                <p className="my-2 text-p-base text-content">
-                  {slideData.name}
-                </p>
-                <div className="text-l-lg">
-                  {slideData.discount > 0 ? (
-                    <span className="me-2 text-accent-yellow">
-                      {slideData.discount}%
-                    </span>
-                  ) : (
-                    ""
-                  )}
-                  <span className="text-content">
-                    {comma(
-                      slideData.price -
-                        Math.floor(
-                          (slideData.price * (slideData.discount * 0.01)) / 10
-                        ) *
-                          10
-                    )}
-                    원
+              <SwiperSlide key={id} className="w-[249px]">
+                <a href="#" className="inline-block m-0.5">
+                  <div className="h-[320px] overflow-hidden">
+                    <img
+                      className="w-full"
+                      src={`../src/assets/product/product_thumb${src}.webp`}
+                      alt={alt}
+                    />
+                  </div>
+                  <span className="inline-block mt-4 text-l-sm text-gray-400">
+                    샛별배송
                   </span>
-                  {slideData.discount > 0 ? (
-                    <del className="block mt-2 text-p-sm text-gray-400">
-                      {comma(slideData.price)}원
-                    </del>
-                  ) : (
-                    ""
-                  )}
-                  <p className="mt-2 text-p-sm text-gray-400">
-                    {slideData.detail}
-                  </p>
-                </div>
+                  <p className="my-2 text-p-base text-content">{name}</p>
+                  <div className="text-l-lg">
+                    {discount > 0 ? (
+                      <span className="me-2 text-accent-yellow">
+                        {discount}%
+                      </span>
+                    ) : (
+                      ""
+                    )}
+                    <span className="text-content">
+                      {comma(
+                        price -
+                          Math.floor((price * (discount * 0.01)) / 10) * 10
+                      )}
+                      원
+                    </span>
+                    {discount > 0 ? (
+                      <del className="block mt-2 text-p-sm text-gray-400">
+                        {comma(price)}원
+                      </del>
+                    ) : (
+                      ""
+                    )}
+                    <p className="mt-2 text-p-sm text-gray-400">{detail}</p>
+                  </div>
+                </a>
               </SwiperSlide>
             );
           })}
@@ -76,24 +72,3 @@ export default function RollingBanner() {
     </>
   );
 }
-
-// function Exercise() {
-//   return (
-//     /* JSX 영역에서는 식만 사용 가능 */
-//     <ul>
-//       {bannerData.items?.map(({ id, gender, face, name, isOnline }) => {
-//         const photoUrl = `/images/faces/${gender}-0${face}.png`;
-
-//         // function body 영역
-//         // 문, 식 모두 사용 가능
-
-//         return (
-//           /* JSX 영역에서는 식만 사용 가능 */
-//           <li key={id}>
-//             <Avatar photo={photoUrl} name={name} isOnline={isOnline} />
-//           </li>
-//         );
-//       })}
-//     </ul>
-//   );
-// }
